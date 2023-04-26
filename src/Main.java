4526faff62f9830fd24572faa3d730e4265b4573
ceewos <118019825+ceewos@users.noreply.github.com>
2023-04-26 15:53:09 -0600

    One GUI

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
         new Controller(8000).start();
-        new Controller(8080).start();
+        //new Controller(8080).start();
 
     }
 }
